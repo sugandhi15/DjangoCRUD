d6a0f38a9d792ddcfad2ae3c4c2f9f866373fdfb
sugandhi15 <sugandhibansal26@gmail.com>
2024-10-06 09:41:04 +0530

    set allowed hosts

--- cruddjango/settings.py
+++ cruddjango/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-)#=27c^s=!4a#sh%3^)mh_)zix+ihmp#il1kpaxhqo6qcc3og$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
